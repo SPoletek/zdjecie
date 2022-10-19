c80cb84baf57fc6fb148579f502f95d932a7fa2a
ogkamilek <kamil.kozlowski.hel@gmail.com>
2022-10-19 15:11:54 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA AAUAAAAAAAAAAAAAAAA AAHUAJAAAAAAAAAAAAAAAAAAAAAAAAHUJ

--- app/src/main/java/com/example/zdjecie/MainActivity.kt
+++ app/src/main/java/com/example/zdjecie/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.zdjecie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.SeekBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
